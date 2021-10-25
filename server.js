5c4ca9d8dcefd0223744c75d2bd6f5c9ff17e7be
JoseEPina <jose.edpina@gmail.com>
2021-10-25 00:04:39 -0600

    Added handlebars files and defined template engine on server.js, created views folder, main.handlebars, homepage.handlebars. Also renamed routes folder to controllers to match MVC structure of updated references in server.js and tested view homepage.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const path = require('path'); // path module utility to work w/ file and directory paths
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 const exphbs = require('express-handlebars'); // to set up handlebars as the
@@ -13,6 +13,9 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', hbs.engine); // sets express engine 'handlebars' from handlebars' engine
+app.set('view engine', 'handlebars'); // sets 'view engine' from app.engine
 
 // turn on routes LAST, after adding middle ware above.
 app.use(routes);
