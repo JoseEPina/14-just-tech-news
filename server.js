53117babe00842c7a7b79f530ebb476af44f7b86
JoseEPina <jose.edpina@gmail.com>
2021-10-24 22:59:10 -0600

    Added Connection and Route to style.css file.

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
+const path = require('path'); // path module utility to work w/ file and directory paths
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
 
+const exphbs = require('express-handlebars'); // to set up handlebars as the
+const hbs = exphbs.create({}); // app's template engine of choice
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// turn on routes
+// turn on routes LAST, after adding middle ware above.
 app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-   app.listen(PORT, () => console.log('Now listening'));
+   app.listen(PORT, () => console.log(`Now listening at ${PORT}`));
 });
